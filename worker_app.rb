2fd927a7a40fcae79fdedbfb1fe7df152fc6de9a
Kim SAVAROCHE <kimsavinfo@air-de-kim.home>
2015-03-24 01:16:45 +0100

    v1 OK

--- worker_app.rb
+++ worker_app.rb
@@ -1,5 +1,5 @@
 require './workers/init_worker'
-require './workers/test_worker'
+require './workers/youtube_crawler_worker'
 require 'ashikawa-core'
 require 'yell'
 require 'redis'
@@ -14,30 +14,42 @@ arangodb = Ashikawa::Core::Database.new do |config|
   config.logger = logger
 end
 
-worker_pool = Thread.new do
-  while true do
-    person = redis.rpop('workers')
+person = {'name' => 'k2r79'}
 
-    if person
-      # Parse the JSON
-      person = JSON.parse(person)
+# Run the initialization worker
+init_worker = InitWorker.new(arangodb, person)
+init_worker.run
 
-      # Run the initialization worker
-      init_worker = InitWorker.new(arangodb, person)
-      init_worker.run
+person['key'] = init_worker.person.key
 
-      # Set the ArangoDB document key
-      person['key'] = init_worker.person.key
+youtube_crawler_worker = YoutubeCrawlerWorker.new(arangodb, person)
+youtube_crawler_worker.run
 
-      # Add workers here
-      ##### EXAMPLE ######
-      Thread.new do
-        test_worker = TestWorker.new(arangodb, person)
-        test_worker.run
-      end
-      ##### END EXAMPLE ######
-    end
-  end
-end
 
-worker_pool.join
+# worker_pool = Thread.new do
+#   while true do
+#     person = redis.rpop('workers')
+
+#     if person
+#       # Parse the JSON
+#       person = JSON.parse(person)
+
+#       # Run the initialization worker
+#       init_worker = InitWorker.new(arangodb, person)
+#       init_worker.run
+
+#       # Set the ArangoDB document key
+#       person['key'] = init_worker.person.key
+
+#       # Add workers here
+#       ##### EXAMPLE ######
+#       Thread.new do
+#         youtube_crawler_worker = YoutubeCrawlerWorker.new(arangodb, person)
+#         youtube_crawler_worker.run
+#       end
+#       ##### END EXAMPLE ######
+#     end
+#   end
+# end
+
+# worker_pool.join
---
+++ workers/youtube_crawler_worker.rb
@@ -0,0 +1,46 @@
+require './workers/worker'
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+class YoutubeCrawlerWorker < Worker
+  def initialize(database, person)
+    super(database, person)
+
+    @name = 'Youtube Crawler Worker'
+  end
+
+  def job
+    puts @person['name']
+
+    #youtube_uri = 'http://gdata.youtube.com/feeds/api/users/' + @person['name'] + '/uploads?v=2&format=5&max-results=5'
+    youtube_uri = 'http://gdata.youtube.com/feeds/api/users/' + @person['name'] + '/uploads?v=2&format=5'
+    youtube_data = Nokogiri::XML(open(youtube_uri)) 
+
+    youtube_data.search('entry').each do |entry|
+
+      puts entry.at('author').at('name').text
+      puts entry.at('author').at('uri').text
+      puts entry.at('author').at('.//yt:userId').text
+      puts entry.at('title').text
+      puts entry.at('.//media:description').text
+      puts entry.at('published').text
+      puts entry.at('updated').text
+      puts entry.at('.//media:category').text
+      puts entry.at('.//media:credit').text
+      puts entry.at('.//media:player').attribute('url')
+      puts entry.at('.//media:content').attribute('duration')
+
+      puts entry.at('.//yt:statistics').attribute('viewCount')
+      puts entry.at('.//yt:statistics').attribute('favoriteCount')
+      if ( entry.at('.//yt:rating') )
+        puts entry.at('.//yt:rating').attribute('numDislikes')
+        puts entry.at('.//yt:rating').attribute('numLikes')
+      else
+        puts "0"
+        puts "0"
+      end
+    end
+
+  end
+end
